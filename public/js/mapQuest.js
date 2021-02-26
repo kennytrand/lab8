b346032c4886f3972fc4ce2806ef63353bf1fd7d
Kenny Tran <k3tran@ucsd.edu>
2021-02-25 16:47:00 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'SORWpNJym8MASGxdhwhnSlMKbTtKD8zc';
+
+// 'map' refers to a <div> element with the ID map
+var map = L.mapquest.map('map', {
+  center: [32.87864642544405, -117.2358752727495],
+  layers: L.mapquest.tileLayer('map'),
+  zoom: 17,
+  zoomControl: false
+});
+L.marker([32.878610384176795, -117.23589673042142]).addTo(map);
 }
